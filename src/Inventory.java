02b00ba8c3a0553ebe9083264ff0ff0fa57987a2
Tomi <pintertamas99@gmail.com>
2021-03-17 14:49:44 +0100

    létre lett hozva az inventory és a portal osztálya

---
+++ src/Inventory.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Inventory {
+    ArrayList<Portal> portals;
+    ArrayList<Material> materials;
+
+    public void addMaterial(Material m) {}
+    public void removeMaterial(Material m) {}
+    public void addPortal(Portal p) {}
+    public void removePortal(Portal p) {}
+}
