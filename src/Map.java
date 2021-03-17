5e0b963f2fef2d7152b96a78f38f7bf1faef07dc
Tomi <pintertamas99@gmail.com>
2021-03-17 15:05:29 +0100

    minden megvan egy helyen egyszerre

--- src/Map.java
+++ src/Map.java
@@ -4,23 +4,40 @@ public class Map {
     ArrayList<Asteroid> asteroids;
 
 
-    public void addAsteroid(asteroids a){}
-    public void removeAsteroid(asteroids a){}
-    public boolean hasAllMaterials(){
+    public void addAsteroid(Asteroid a) {
+    }
+
+    public void removeAsteroid(Asteroid a) {
+    }
+
+    public boolean hasAllMaterials() {
         return true;
     }
-    public void initGame(int numberOfPlayers){}
-    public void solarStorm(){}
-    public boolean checkGameEnd(){
+
+    public void initGame(int numberOfPlayers) {
+    }
+
+    public void solarStorm() {
+    }
+
+    public boolean checkGameEnd() {
         return true;
     }
-    public boolean checkIfWinnable(){
+
+    public boolean checkIfWinnable() {
         return true;
     }
-    public void SetupRound(){}
-    public void gameEnd(boolean b){}
-    public boolean stormComing(){
+
+    public void SetupRound() {
+    }
+
+    public void gameEnd(boolean b) {
+    }
+
+    public boolean stormComing() {
         return true;
     }
-    public void reset(){}
+
+    public void reset() {
+    }
 }
