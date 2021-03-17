61d1d4c34c67bed3d6093150aa4f7eb72c5c602e
Richárd Bödőcs <bodocs.richard@edu.bme.hu>
2021-03-17 14:56:22 +0100

    billof and map

---
+++ src/BillOfMaterials.java
@@ -0,0 +1,15 @@
+public class BillOfMaterials {
+
+    ArrayList<Material> materials;
+
+    public void initMaterials(materials m){}
+    public void addMaterial(materials m){}
+    public boolean hasEnoughMaterial(materials m){
+        return true;
+    }
+    public boolean isNedeed(materials m){
+        return true;
+    }
+    public void checkMaterials(materials m){}
+    public void resetBill(){}
+}
---
+++ src/Map.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class Map {
+    ArrayList<Asteroid> asteroids;
+
+
+    public void addAsteroid(asteroids a){}
+    public void removeAsteroid(asteroids a){}
+    public boolean hasAllMaterials(){
+        return true;
+    }
+    public void initGame(int numberOfPlayers){}
+    public void solarStorm(){}
+    public boolean checkGameEnd(){
+        return true;
+    }
+    public boolean checkIfWinnable(){
+        return true;
+    }
+    public void SetupRound(){}
+    public void gameEnd(boolean b){}
+    public boolean stormComing(){
+        return true;
+    }
+    public void reset(){}
+}
