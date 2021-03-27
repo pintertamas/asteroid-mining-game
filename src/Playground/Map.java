fd01b08f018791a6408646ced1eb9ef7ee494c35
SteveP1999 <halabe@freemail.hu>
2021-03-27 15:25:28 +0100

    Választható, hogy auto generate vagy manual

--- src/Playground/Map.java
+++ src/Playground/Map.java
@@ -48,166 +48,228 @@ public class Map {
     public void initGame(int numberOfPlayers) {
         TestLogger.functionCalled(this, "initGame", "int numberOfPlayers", "void");
 
-        //Változók amelyeket a player inputnál használunk fel
-        int numberOfAsteroid = -1;
-        int mat = -1;
-        int sun = -1;
-        int hollow = -1;
-        int layer = -1;
-        int numberOfNeighbours = -1;
-        boolean nearSun = false;
-        boolean isHollow = false;
-        int neighbour = -1;
-        int numberOfUfos = -1;
-        int ufoAsteroid = -1;
-        int numberOfSettlers = -1;
-        int settlerAsteroid = -1;
-        ArrayList<Integer> nums = new ArrayList<>();
-
-        //Itt lehet az asteroidák számát megadni
-        System.out.println("Mennyi asteroidát szeretnél a pályára, add meg:");
-        Scanner kb = new Scanner(System.in);
-        if (kb.hasNextInt()) {
-            numberOfAsteroid = kb.nextInt();
+        System.out.println("Which option would you like to choose?");
+        System.out.println("Generat objects automatically - Press 1");
+        System.out.println("Generat objects manually - Press 0");
+        int input = -1;
+        Scanner sc = new Scanner(System.in);
+        while (input < 0 || input > 1) {
+            if (sc.hasNextInt()) {
+                input = sc.nextInt();
+            }
         }
+        if (input == 0) {
+            //Változók amelyeket a player inputnál használunk fel
+            int numberOfAsteroid = -1;
+            int mat = -1;
+            int sun = -1;
+            int hollow = -1;
+            int layer = -1;
+            int numberOfNeighbours = -1;
+            boolean nearSun = false;
+            boolean isHollow = false;
+            int neighbour = -1;
+            int numberOfUfos = -1;
+            int ufoAsteroid = -1;
+            int numberOfSettlers = -1;
+            int settlerAsteroid = -1;
+            ArrayList<Integer> nums = new ArrayList<>();
 
-
-        //Az asteroidák belső tulajdonságainak az eldöntése:
-        for (int i = 0; i < numberOfAsteroid; i++) {
-            while (mat < 0 || mat > 3) {
-                System.out.println("Az " + i + ". asteroidának milyen legyen a belseje?: ");
-                System.out.println("0 - Uranium");
-                System.out.println("1 - Ice");
-                System.out.println("2 - Coal");
-                System.out.println("3 - Iron");
-                kb = new Scanner(System.in);
-                if (kb.hasNextInt()) {
-                    mat = kb.nextInt();
-                }
-            }
-            Material m = null;
-            switch (mat) {
-                case 0:
-                    m = new Uranium();
-                case 1:
-                    m = new Ice();
-                    break;
-                case 2:
-                    m = new Coal();
-                    break;
-                case 3:
-                    m = new Iron();
-                    break;
-            }
-
-            //Napközelben legyen-e vagy sem az asteroida:
-            while (sun < 0 || sun > 1) {
-                System.out.println("Napközelben legyen az asteroida?");
-                System.out.println("1 - Igen");
-                System.out.println("0 - Nem");
-                kb = new Scanner(System.in);
-                if (kb.hasNextInt()) {
-                    sun = kb.nextInt();
-                }
-            }
-            switch (sun) {
-                case 0:
-                    nearSun = false;
-                case 1:
-                    nearSun = true;
-                    break;
-                default:
-                    System.out.println("Not a valid number dummy.");
-            }
-
-
-            while (hollow < 0 || hollow > 1) {
-                System.out.println("Üreges legyen az asteroida? ");
-                System.out.println("0 - Nem");
-                System.out.println("1 - Igen");
-                kb = new Scanner(System.in);
-                if (kb.hasNextInt()) {
-                    hollow = kb.nextInt();
-                }
-            }
-            switch (hollow) {
-                case 0:
-                    isHollow = false;
-                case 1:
-                    isHollow = true;
-                    break;
-                default:
-                    System.out.println("Not a valid number dummy.");
-            }
-
-            //Mennyi legyen a layer az asteroidán:
-            System.out.println("Milyen vastag legyen az asteroida köpenye?");
-            kb = new Scanner(System.in);
+            //Itt lehet az asteroidák számát megadni
+            System.out.println("Mennyi asteroidát szeretnél a pályára, add meg:");
+            Scanner kb = new Scanner(System.in);
             if (kb.hasNextInt()) {
-                layer = kb.nextInt();
+                numberOfAsteroid = kb.nextInt();
             }
 
-            //Asteroida létrehozása:
-            Asteroid ast = new Asteroid(m, layer, nearSun, isHollow);
-            asteroids.add(ast);
 
-            //Szomszédok beállítása számának beállítása:
-            for (int j = 0; j < numberOfAsteroid; j++) {
-                System.out.println("A(z) " + j + ". asteroidának hány szomszédja legyen?");
-                while (numberOfNeighbours < 0 || numberOfNeighbours > numberOfAsteroid - 1) {
+            //Az asteroidák belső tulajdonságainak az eldöntése:
+            for (int i = 0; i < numberOfAsteroid; i++) {
+                while (mat < 0 || mat > 3) {
+                    System.out.println("Az " + i + ". asteroidának milyen legyen a belseje?: ");
+                    System.out.println("0 - Uranium");
+                    System.out.println("1 - Ice");
+                    System.out.println("2 - Coal");
+                    System.out.println("3 - Iron");
                     kb = new Scanner(System.in);
                     if (kb.hasNextInt()) {
-                        numberOfNeighbours = kb.nextInt();
+                        mat = kb.nextInt();
                     }
                 }
+                Material m = null;
+                switch (mat) {
+                    case 0:
+                        m = new Uranium();
+                    case 1:
+                        m = new Ice();
+                        break;
+                    case 2:
+                        m = new Coal();
+                        break;
+                    case 3:
+                        m = new Iron();
+                        break;
+                }
 
-                //Egyesével beállítani ki legyen a szomszéd:
-                for (int k = 0; k < numberOfNeighbours; k++) {
-                    System.out.println("Mi legyen a" + k + ". szomszád, adj meg egy asteroidát a sorszámával.");
-                    while (neighbour < 0 || neighbour > numberOfAsteroid - 1 || !(canBePlaced(neighbour, nums)) || k == neighbour) {
+                //Napközelben legyen-e vagy sem az asteroida:
+                while (sun < 0 || sun > 1) {
+                    System.out.println("Napközelben legyen az asteroida?");
+                    System.out.println("1 - Igen");
+                    System.out.println("0 - Nem");
+                    kb = new Scanner(System.in);
+                    if (kb.hasNextInt()) {
+                        sun = kb.nextInt();
+                    }
+                }
+                switch (sun) {
+                    case 0:
+                        nearSun = false;
+                    case 1:
+                        nearSun = true;
+                        break;
+                    default:
+                        System.out.println("Not a valid number dummy.");
+                }
+
+
+                while (hollow < 0 || hollow > 1) {
+                    System.out.println("Üreges legyen az asteroida? ");
+                    System.out.println("0 - Nem");
+                    System.out.println("1 - Igen");
+                    kb = new Scanner(System.in);
+                    if (kb.hasNextInt()) {
+                        hollow = kb.nextInt();
+                    }
+                }
+                switch (hollow) {
+                    case 0:
+                        isHollow = false;
+                    case 1:
+                        isHollow = true;
+                        break;
+                    default:
+                        System.out.println("Not a valid number dummy.");
+                }
+
+                //Mennyi legyen a layer az asteroidán:
+                System.out.println("Milyen vastag legyen az asteroida köpenye?");
+                kb = new Scanner(System.in);
+                if (kb.hasNextInt()) {
+                    layer = kb.nextInt();
+                }
+
+                //Asteroida létrehozása:
+                Asteroid ast = new Asteroid(m, layer, nearSun, isHollow);
+                asteroids.add(ast);
+
+                //Szomszédok beállítása számának beállítása:
+                for (int j = 0; j < numberOfAsteroid; j++) {
+                    System.out.println("A(z) " + j + ". asteroidának hány szomszédja legyen?");
+                    while (numberOfNeighbours < 0 || numberOfNeighbours > numberOfAsteroid - 1) {
                         kb = new Scanner(System.in);
                         if (kb.hasNextInt()) {
-                            neighbour = kb.nextInt();
+                            numberOfNeighbours = kb.nextInt();
                         }
                     }
-                    asteroids.get(k).addNeighbor(asteroids.get(neighbour));
+
+                    //Egyesével beállítani ki legyen a szomszéd:
+                    for (int k = 0; k < numberOfNeighbours; k++) {
+                        System.out.println("Mi legyen a" + k + ". szomszád, adj meg egy asteroidát a sorszámával.");
+                        while (neighbour < 0 || neighbour > numberOfAsteroid - 1 || !(canBePlaced(neighbour, nums)) || k == neighbour) {
+                            kb = new Scanner(System.in);
+                            if (kb.hasNextInt()) {
+                                neighbour = kb.nextInt();
+                            }
+                        }
+                        asteroids.get(k).addNeighbor(asteroids.get(neighbour));
+                    }
+                }
+
+                //Ufok létrehozása:
+                System.out.println("Mennyi ufot szeretnél szeretnél a pályára, add meg:");
+                kb = new Scanner(System.in);
+                if (kb.hasNextInt()) {
+                    numberOfUfos = kb.nextInt();
+                }
+                for (int j = 0; j < numberOfUfos; j++) {
+                    System.out.println("A(z) " + j + ". ufo melyik asteroidán legyen, add meg a sorszámát:");
+                    while (ufoAsteroid < 0 || ufoAsteroid > numberOfAsteroid) {
+                        kb = new Scanner(System.in);
+                        if (kb.hasNextInt()) {
+                            ufoAsteroid = kb.nextInt();
+                        }
+                    }
+                    Ufo f = new Ufo(asteroids.get(ufoAsteroid), false);
+                    asteroids.get(ufoAsteroid).addFigure(f);
+                }
+
+                //Settlerek létrehozása:
+                for (int j = 0; j < numberOfPlayers; j++) {
+                    System.out.println("A(z) " + j + ". settler melyik asteroidán legyen, add meg a sorszámát:");
+                    while (settlerAsteroid < 0 || settlerAsteroid > numberOfAsteroid) {
+                        kb = new Scanner(System.in);
+                        if (kb.hasNextInt()) {
+                            settlerAsteroid = kb.nextInt();
+                        }
+                    }
+                    Settler s = new Settler(asteroids.get(settlerAsteroid), false);
+                    asteroids.get(settlerAsteroid).addFigure(s);
+                }
+            }
+        }
+        else {
+            int minimumNumberOfAsteroids = 5;
+            int maximumNumberOfAsteroids = 20;
+            double numberOfAsteroids = Math.random() * (maximumNumberOfAsteroids - minimumNumberOfAsteroids + 1) + minimumNumberOfAsteroids;
+            for (int i = 0; i < numberOfAsteroids; i++) {
+                Random rand = new Random();
+                int typeOfAsteroid = rand.nextInt(4);
+                int numberOfLayers = rand.nextInt(5);
+                Material m;
+                switch (typeOfAsteroid) {
+                    case 0:
+                        m = new Iron();
+                        break;
+                    case 1:
+                        m = new Coal();
+                        break;
+                    case 2:
+                        m = new Ice();
+                        break;
+                    default:
+                        m = new Uranium();
+                }
+                int boolOfSun = rand.nextInt(2);
+                boolean sun;
+                sun = boolOfSun != 0;
+                Asteroid a = new Asteroid(m, numberOfLayers, sun, false);
+                this.asteroids.add(a);
+            }
+
+            for (int i = 0; i < numberOfAsteroids; i++) {
+                if (i < numberOfAsteroids - 1) {
+                    asteroids.get(i).addNeighbor(asteroids.get(i + 1));
+                    asteroids.get(i + 1).addNeighbor(asteroids.get(i));
+                } else {
+                    asteroids.get(i).addNeighbor(asteroids.get(0));
+                    asteroids.get(0).addNeighbor(asteroids.get(i));
                 }
             }
 
-            //Ufok létrehozása:
-            System.out.println("Mennyi ufot szeretnél szeretnél a pályára, add meg:");
-            kb = new Scanner(System.in);
-            if (kb.hasNextInt()) {
-                numberOfUfos = kb.nextInt();
-            }
-            for (int j = 0; j < numberOfUfos; j++) {
-                System.out.println("A(z) " + j + ". ufo melyik asteroidán legyen, add meg a sorszámát:");
-                while (ufoAsteroid < 0 || ufoAsteroid > numberOfAsteroid) {
-                    kb = new Scanner(System.in);
-                    if (kb.hasNextInt()) {
-                        ufoAsteroid = kb.nextInt();
-                    }
+            //Kisorsol 20 random asteroida párt, felveszi egymásnak őket.
+            for (int i = 0; i < 20; i++) {
+                Random rand = new Random();
+                int a1 = rand.nextInt((int) numberOfAsteroids);
+                int a2 = rand.nextInt((int) numberOfAsteroids);
+                if (a1 != a2 || asteroids.get(a1).getNeighbors().contains(asteroids.get(a2))) {
+                    asteroids.get(a1).addNeighbor(asteroids.get(a2));
+                    asteroids.get(a2).addNeighbor(asteroids.get(a1));
                 }
-                Ufo f = new Ufo(asteroids.get(ufoAsteroid), false);
-                asteroids.get(ufoAsteroid).addFigure(f);
             }
 
-            //Settlerek létrehozása:
-            System.out.println("Mennyi settlert szeretnél a pályára, add meg:");
-            kb = new Scanner(System.in);
-            if (kb.hasNextInt()) {
-                numberOfSettlers = kb.nextInt();
-            }
-            for (int j = 0; j < numberOfSettlers; j++) {
-                System.out.println("A(z) " + j + ". settler melyik asteroidán legyen, add meg a sorszámát:");
-                while (settlerAsteroid < 0 || settlerAsteroid > numberOfAsteroid) {
-                    kb = new Scanner(System.in);
-                    if (kb.hasNextInt()) {
-                        settlerAsteroid = kb.nextInt();
-                    }
-                }
-                Settler s = new Settler(asteroids.get(settlerAsteroid), false);
-                asteroids.get(settlerAsteroid).addFigure(s);
+            for (int i = 0; i < numberOfPlayers; i++) {
+
+                Settler s = new Settler(asteroids.get(0), false);
             }
         }
 
