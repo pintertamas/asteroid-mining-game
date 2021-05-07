f611761a498772de0870aaebea66798fc9a455cb
SteveP1999 <halabe@freemail.hu>
2021-05-07 19:15:22 +0200

    menü az már úgy ahogy megvan

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
 import Controllers.Controller;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 
@@ -11,30 +20,6 @@ import javafx.stage.Stage;
  */
 @SuppressWarnings("SpellCheckingInspection")
 public class Main extends Application {
-    @Override
-    public void start(Stage primaryStage) {
-        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
-        System.out.println(screenBounds);
-        double screenWidth = screenBounds.getWidth();
-        double screenHeight = screenBounds.getHeight();
-        primaryStage.setX(screenBounds.getMinX());
-        primaryStage.setY(screenBounds.getMinY());
-        primaryStage.setWidth(screenWidth);
-        primaryStage.setHeight(screenHeight);
-        primaryStage.setMaximized(true);
-        Group root = new Group();
-
-        Scene scene = new Scene(root);
-        primaryStage.setTitle("Asteroid Mining Controllers.Game");
-        primaryStage.setScene(scene);
-        primaryStage.setResizable(false);
-        primaryStage.show();
-        scene.getStylesheets().add("style.css");
-        root.getStylesheets().addAll("style.css");
-
-        Controller controller = new Controller(root);
-        controller.getGame().run(root, screenBounds);
-    }
 
     /**
      * main függvény.
@@ -44,5 +29,72 @@ public class Main extends Application {
     @SuppressWarnings("SpellCheckingInspection")
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        Stage window = primaryStage;
+        Scene menuScene;
+
+        Group root = new Group();
+        Scene scene = new Scene(root);
+
+        Rectangle2D screenBounds = Screen.getPrimary().getBounds();
+        System.out.println(screenBounds);
+        double screenWidth = screenBounds.getWidth();
+        double screenHeight = screenBounds.getHeight();
+        primaryStage.setX(screenBounds.getMinX());
+        primaryStage.setY(screenBounds.getMinY());
+        primaryStage.setWidth(screenWidth);
+        primaryStage.setHeight(screenHeight);
+        primaryStage.setMaximized(true);
+        primaryStage.setTitle("Asteroid Mining Controllers.Game");
+        //primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
+        //primaryStage.show();
+        scene.getStylesheets().add("style.css");
+        root.getStylesheets().addAll("style.css");
+
+        Controller controller = new Controller(root);
+        controller.getGame().run(root, screenBounds);
+
+        Button button1 = new Button("Play");
+        Button button2 = new Button(("Options"));
+        Button button3 = new Button("Quit");
+        Font font = Font.font("Courier New", FontWeight.BOLD, 36);
+        button1.setFont(font);
+
+        VBox vBox = new VBox(20);
+
+        StackPane stackPane1 = new StackPane();
+        stackPane1.getChildren().addAll(button1);
+        StackPane stackPane2 = new StackPane();
+        stackPane2.getChildren().addAll(button2);
+        StackPane stackPane3 = new StackPane();
+        stackPane3.getChildren().addAll(button3);
+
+        Image image = new Image("background.png");
+        ImageView imageView = new ImageView(image);
+        Group group = new Group();
+        vBox.getChildren().addAll(stackPane1, stackPane2, stackPane3);
+        group.getChildren().addAll(imageView, vBox);
+
+        menuScene = new Scene(group, 200, 200);
+        menuScene.getStylesheets().add("style.css");
+
+        button1.setOnAction(e -> primaryStage.setScene(scene));
+        button3.setOnAction(e -> System.exit(0));
+
+        button1.setId("playButton");
+        button2.setId("optionsButton");
+        button3.setId("quitButton");
+
+
+
+        window.setScene(menuScene);
+        window.setTitle("Asteroid mining game");
+        window.show();
+
     }
 }
